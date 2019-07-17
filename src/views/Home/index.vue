01b2796ae719770cd2b3520ba8f98e0ec38f9c04
hyy <2326093449@qq.com>
2019-07-17 21:27:12 +0800

    左侧菜单栏

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,58 +1,88 @@
 <template>
-  <el-container class="home-container">
-    <!-- 头部 -->
-    <el-header>
-     <div>
-        <img src="./heima.png" alt="">
-        <span>电商后台管理系统</span>
-      </div>
-      <el-button type="info" @click='clear'>退出</el-button>
-    </el-header>
-    <!-- 主体 -->
-    <el-container>
-      <!-- 侧边栏 -->
-      <el-aside width="200px">
-        <!-- 导航栏 -->
-        <el-menu background-color="#001529" text-color="#fff" active-text-color="#ffd04b">
-        <!-- 一级菜单 -->
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <!-- 二级菜单 -->
-          <el-menu-item index="1-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-      <!-- 右侧内容主体 -->
-      <el-main>Main</el-main>
+    <el-container class="home-container">
+      <!-- 头部 -->
+      <el-header>
+        <div>
+          <img src="./heima.png" alt="">
+          <span>电商后台管理系统</span>
+        </div>
+        <el-button type="info" @click='clear'>退出</el-button>
+      </el-header>
+      <el-container>
+        <!-- 左侧导航栏 -->
+        <el-aside :width="width">
+          <div class="toggle-button" @click='toggle'>|||</div>
+          <!-- 侧边栏菜单 -->
+          <el-menu background-color="#333744" text-color="#fff"
+           active-text-color="#409fff" :unique-opened="true" :collapse="iscollapse" :collapse-transition="false">
+             <!-- 一级菜单 -->
+            <el-submenu :index="menu.id+''" v-for="menu in menuslist" :key="menu.id">
+              <!-- 一级菜单模板区域 -->
+              <template slot="title">
+                <i :class="iconsObj[menu.id]"></i>
+                <span>{{menu.authName}}</span>
+              </template>
+
+                <el-menu-item :index="subMenu.id+''"  v-for="subMenu in menu.children" :key="subMenu.id">
+                  <!-- 二级菜单 -->
+                  <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>{{subMenu.authName}}</span>
+                  </template>
+                </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <!-- 右侧内容 -->
+        <el-main>Main</el-main>
+      </el-container>
     </el-container>
-  </el-container>
 </template>
 <script>
 export default {
-  data: () => ({
-    menus: []
-  }),
+  data () {
+    return {
+      menuslist: [],
+      iconsObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      iscollapse: false
+
+    }
+  },
   created () {
-    this.getMenus()
+    this.getMenuList()
   },
   methods: {
     clear () {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
-    async getMenus () {
-      const { data: { data, meta } } = await this.$http.get('menus')
-      if (meta.status !== 200) return this.$message.error('获取失败')
-      console.log(data)
-      this.$message.success('获取成功')
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error('获取数据失败')
+      // 请求到的数据传给menuslist
+      this.menuslist = res.data
+    },
+    // 切换左侧栏折叠状态
+    toggle () {
+      this.iscollapse = !this.iscollapse
+      // console.log(this.iscollapse)
+    }
+  },
+  computed: {
+    width () {
+      // 根据iscollapse修改侧边栏宽度
+      return this.iscollapse ? '60px' : '200px'
     }
   }
 }
 </script>
-<style  lang="less" scoped>
+<style lang="less" scoped>
 .home-container {
   height: 100%;
 }
