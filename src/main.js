6594beeb3af1ec3e2b180be3806eb01d13dd2511
hyy <2326093449@qq.com>
2019-07-17 08:31:31 +0800

    预验证

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入字体 图标
+import './assets/fonts/iconfont.css'
+// 全局less配置
 import './assets/css/base.less'
 import './plugins/element.js'
 // // 手动配置element - ui
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
 // Vue.use(ElementUI)
+
+import axios from 'axios'
+// 默认根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,68 +1,117 @@
 <template>
+import { async } from 'q';
   <div id="login_container" class="login_container">
     <div class="login-box">
+      <!-- 头像 -->
       <div class="avatar-box">
         <img src="./logo.png" alt="">
       </div>
-      <el-form>
-        <el-form-item>
-          <el-input class="login-form"></el-input>
+      <!-- 登录表单 -->
+      <el-form class="login_form" :rules="loginRules" :model="loginFrom" ref="loginFromRef">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input placeholder="用户名" prefix-icon="iconfont icon-user" v-model="loginFrom.username"></el-input>
         </el-form-item>
-
-        <el-form-item>
-          <el-input class="login-form"></el-input>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input type="password" placeholder="密码" prefix-icon="iconfont icon-3702mima" v-model="loginFrom.password"></el-input>
         </el-form-item>
-
-        <el-form-item>
-        <el-button type="primary">登录</el-button>
-        <el-button RESET>重置</el-button>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="resetFrom">重置</el-button>
       </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 <script>
-export default {}
-</script>
-<style lang="less" scoped>
-.login_container{
-height: 100%;
-  background: #2b4b6b;
-  .login-box {
-    width: 450px;
-    height: 300px;
-    background-color: #fff;
-    border-radius: 3px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
 
- .avatar-box{
-      height: 130px;
-      width: 130px;
-      border: 1px solid #eee;
-      border-radius: 50%;
-      padding: 10px;
-      box-shadow: 0 0 10px #ddd;
-      position: absolute;
-      left: 50%;
-      transform: translate(-50%,-50%);
-      background-color: #fff;
-      img{
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-      border: 1px solid #eee;
-        background-color: #fff;
-      }
+export default {
+  data: () => ({
+    // 登录表单数据绑定
+    loginFrom: {
+      username: '1',
+      password: '1'
+    },
+    // 规则验证
+    loginRules: {
+      username: [
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+      ],
+      password: [
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+      ]
     }
-    .login-form{
-          padding: 20px;
-          width: 90%;
-          top: 68px;
+  }),
+  methods: {
+    // 点击重置按钮 重置表单内容
+    resetFrom () {
+      console.log(this)
+      this.$refs.loginFromRef.resetFields()
+      this.loginFrom.username = this.loginFrom.password = ''
+    },
+    // 点击登录按钮
+    login () {
+      // 表单预验证
+      this.$refs.loginFromRef.validate(async valid => {
+        console.log(valid)
+        if (!valid) return
+        const res = await this.$http.post('login', this.loginFrom)
+        console.log(res)
+      })
     }
-
   }
 }
+</script>
+<style lang="less" scoped>
+    .login_container{
+    height: 100%;
+      background: #2b4b6b;
+      .login-box {
+        width: 450px;
+        height: 300px;
+        background-color: #fff;
+        border-radius: 3px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+
+    .avatar-box{
+        height: 130px;
+        width: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background-color: #fff;
+        img{
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        border: 1px solid #eee;
+          background-color: #fff;
+        }
+      }
+     .login_form {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        padding: 0 20px;
+        box-sizing: border-box;
+      }
+
+      .btns {
+        display: flex;
+        justify-content: flex-end;
+      }
+
+    }
+  }
 </style>
